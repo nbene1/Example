0b3a599227f957c20ae58895a81f4dc9a334788e
Harris <hbadl1@ocdsb.ca>
2026-04-30 11:42:08 -0400

    simba

---
+++ g.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class g here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class g
+{
+}
---
+++ n.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class n here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class n
+{
+}
