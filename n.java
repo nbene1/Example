0662ade33d92d873558b6490dd5c14a87a8241c2
Artem <atche1@ocdsb.ca>
2026-05-01 09:14:52 -0400

    c

--- n.java
+++ n.java
@@ -1,10 +1,14 @@
-
-/**
- * Write a description of class n here.
+/*
+ * Description: 
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class n
-{
+
+public class n {
+    public static void main(String args[]) {
+        for (int i = 0; i == i; i++) {
+            System.out.println("Hello World!");
+        }
+    }
 }
